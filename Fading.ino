93417528a36dcecdf8cd69a4b986143d5d438106
Doug Stevens <physmatical@gmail.com>
2018-03-14 18:25:58 -0600

    Current working

--- Fading.ino
+++ Fading.ino
@@ -1,24 +1,6 @@
-/*
- Fading
-
- This example shows how to fade an LED using the analogWrite() function.
-
- The circuit:
- * LED attached from digital pin 9 to ground.
-
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
-
- http://www.arduino.cc/en/Tutorial/Fading
-
- This example code is in the public domain.
-
- */
 
 
-int redPin = 3;    // LED connected to digital pin 9
+int redPin = 3;
 int greenPin = 5;
 int bluePin = 6;
 int redShade=0;
@@ -34,15 +16,17 @@ void setup() {
 
 void loop() {
   analogWrite(redPin,redShade);
+  delay(40);
   analogWrite(greenPin,greenShade);
+  delay(40);
   analogWrite(bluePin,blueShade);
+  delay(40);
   redShade +=1;
   blueShade+=2;
   greenShade+=3;
   if (redShade>255) redShade=redShade-255;
   if (greenShade>255) greenShade=greenShade-255;
   if (blueShade>255) blueShade=blueShade-255;
-  delay(40);
 }
 
 
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-03-13 19:14:00
+//This file has been generated on 2018-03-14 18:23:40
 
 #include "Arduino.h"
 
